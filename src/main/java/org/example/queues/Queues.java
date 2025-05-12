ce3093d12829b1442dd81c06fe46f71e634b22d5
giorgos-grekis <georgrekis.github@gmail.com>
2025-05-13 00:16:49 +0300

    queues

--- src/main/java/org/example/queues/Queues.java
+++ src/main/java/org/example/queues/Queues.java
@@ -1,11 +1,22 @@
-package org.example.queue;
+package org.example.queues;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
-public class Queue {
+public class Queues {
     public static void main(String[] args) {
-        Queue<Person> queue = new LinkedList<>();
+        Queue<Person> supermarket = new LinkedList<>();
+
+        supermarket.add(new Person("Giorgos", 36));
+        supermarket.add(new Person("Cisu", 21));
+        supermarket.add(new Person("Test", 69));
+
+        System.out.println(supermarket.size());
+        System.out.println(supermarket.peek());
+        System.out.println(supermarket.poll());
+        System.out.println(supermarket.size());
+        System.out.println(supermarket.peek());
     }
 
-    static class Person(String name, int age) {}
+    static record Person(String name, int age) {}
 }
