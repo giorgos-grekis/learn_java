28b8a90140b639ed86b92d9b8250e43862124588
giorgos-grekis <georgrekis.github@gmail.com>
2025-05-13 11:40:32 +0300

    naming convention

---
+++ src/main/java/org/example/generics/Generics.java
@@ -0,0 +1,16 @@
+package org.example.generics;
+
+public class Generics {
+
+    public static void main(String[] args) {
+        /**
+         * Naming Convention
+         * E -> Element (used extensively by the Java)
+         * K -> Key (Used in Map)
+         * N -> Number
+         * T -> Type
+         * V -> Value (Used in Map)
+         */
+    }
+
+}
