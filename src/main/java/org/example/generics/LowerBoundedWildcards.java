1126049a098841a2c0ba517cdce0444b441ce013
giorgos-grekis <georgrekis.github@gmail.com>
2025-05-13 12:56:43 +0300

    LowerBoundedWildcards

--- src/main/java/org/example/generics/LowerBoundedWildcards.java
+++ src/main/java/org/example/generics/LowerBoundedWildcards.java
@@ -9,29 +9,19 @@ public class LowerBoundedWildcards {
         // ? Wildcards
 
         // Unbounded Wildcards
-        List<Double> list1 = Arrays.asList(1.9,1.3);
+//        List<Double> list1 = Arrays.asList(1.9,1.3); // it doesn't work
         List<Integer> list2 = Arrays.asList(1,2);
         List<Number> list3 = Arrays.asList();
-        print(list1);
+//        print(list1);
         print(list2);
         print(list3);
 
 
     }
 
-//    static <T>void print(List<T> list) {
-//        list.forEach(System.out::println);
-//    }
 
-
-//    static void print(List<?> list) {
-//        list.forEach(e -> {
-//            System.out.println(e.getClass().getName());
-//            System.out.println(e);
-//                });
-//    }
-
-    static void print(List<? extends Number> list) {
+    // Number is the super class of Integer
+    static void print(List<? super Integer> list) {
         list.forEach(e -> {
             System.out.println(e.getClass().getName());
             System.out.println(e);
