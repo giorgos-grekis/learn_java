e18b8dc684db80dd1f055a324b43299d3f1832b4
giorgos-grekis <georgrekis.github@gmail.com>
2025-05-13 16:24:39 +0300

    first test

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,4 +1,19 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+
+    @Test
+    void catAddTwoNumbers() {
+        Calculator underTest = new Calculator();
+        int number1 = 10;
+        int number2 = 20;
+
+        int result = underTest.add(number1, number2);
+
+        assertEquals(30, result);
+    }
 }
