1a4ae910ce2621f7d5cb0e0ffade661c6b5205c3
giorgos-grekis <georgrekis.github@gmail.com>
2025-05-13 11:49:35 +0300

    generics and classes

---
+++ src/main/java/org/example/generics/GenericsAndClasses.java
@@ -0,0 +1,16 @@
+package org.example.generics;
+
+public class GenericsAndClasses {
+
+    public static void main(String[] args) {
+        Box<Phone> box = new Box<>();
+        box.set(new Phone("Samsung"));
+        System.out.println(box.get());
+
+        Box<Letter> box2 = new Box<>();
+        box2.set(new Letter("Cisu"));
+        System.out.println(box2.get());
+
+    }
+
+}
