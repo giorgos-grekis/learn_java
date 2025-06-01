3cacf4dc8a31073f5765a803fd9ab8577a677c5e
giorgos-grekis <georgrekis.github@gmail.com>
2025-06-01 13:47:55 +0300

    Strings

---
+++ src/main/java/org/cisu/Strings.java
@@ -0,0 +1,19 @@
+package org.cisu;
+
+public class Strings {
+
+
+    public static void main(String[] args) {
+        //    String message = new String("Hello World");
+        String message = "Hello World" + "!!";
+
+        System.out.println(message.endsWith("!!"));
+        System.out.println(message.startsWith("!!"));
+        System.out.println(message.indexOf("H"));
+        System.out.println(message.indexOf("sky"));
+        System.out.println(message.replace("!", "*"));
+        System.out.println(message);
+        System.out.println(message.toLowerCase());
+        System.out.println(message.toUpperCase());
+    }
+}
