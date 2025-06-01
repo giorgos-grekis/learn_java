574f83a928ec224f7f4fc1ccdf80c6ecc5ac2f45
giorgos-grekis <georgrekis.github@gmail.com>
2025-06-01 13:57:39 +0300

    Multi-dimensional arrays

--- src/main/java/org/cisu/ArraysClass.java
+++ src/main/java/org/cisu/ArraysClass.java
@@ -10,10 +10,19 @@ public class ArraysClass {
 
 //        System.out.println(Arrays.toString(numbers));
 
-        int[] numbers = {2,3,5,1,4};
-        System.out.println(numbers.length);
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
+//        int[] numbers = {2,3,5,1,4};
+//        System.out.println(numbers.length);
+//        Arrays.sort(numbers);
+//        System.out.println(Arrays.toString(numbers));
+
+
+        // Multi-dimensional Arrays
+//        int[][] numbers = new int[2][3];
+//        numbers[0][0] = 1;
+//        System.out.println(Arrays.deepToString(numbers));
+
+        int[][] numbers = {{1,2,3}, {4,5,6}};
+        System.out.println(Arrays.deepToString(numbers));
 
     }
 }
