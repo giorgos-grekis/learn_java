e935ed3c6bf97ee9cd8586fc8e06f250826a7aed
giorgos-grekis <georgrekis.github@gmail.com>
2025-06-04 15:33:12 +0300

    constructors and inheritance

--- src/main/java/org/cisu/Main.java
+++ src/main/java/org/cisu/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         var box1 = new TextBox();
-        var box2 = new TextBox();
-        // return a hash code represent a number for point the memory
-        System.out.println(box1.hashCode());
-        System.out.println(box2.hashCode());
-
-        System.out.println(box1.equals(box2));
+//        var box2 = new TextBox();
+//        // return a hash code represent a number for point the memory
+//        System.out.println(box1.hashCode());
+//        System.out.println(box2.hashCode());
+//
+//        System.out.println(box1.equals(box2));
     }
 }
--- src/main/java/org/cisu/TextBox.java
+++ src/main/java/org/cisu/TextBox.java
@@ -3,6 +3,11 @@ package org.cisu;
 public class TextBox extends UIControl {
     private String text = "";
 
+    public TextBox() {
+        super(true);
+        System.out.println("TextBox");
+    }
+
     public void setText(String text) {
         this.text = text;
     }
