177a56d8f52a0a70641fccc881a9827ad94401b7
giorgos-grekis <georgrekis.github@gmail.com>
2025-06-04 15:28:05 +0300

    the object class

--- src/main/java/org/cisu/Main.java
+++ src/main/java/org/cisu/Main.java
@@ -1,21 +1,14 @@
 package org.cisu;
 
-/** class
- *  A blueprint for creating objects
- */
-
-/** Object
- * An instance of a class
- */
-
-/***
- *  Heap => store Objects
- *  Stack => Primitives and short-lived variables
- */
-
 public class Main {
 
     public static void main(String[] args) {
+        var box1 = new TextBox();
+        var box2 = new TextBox();
+        // return a hash code represent a number for point the memory
+        System.out.println(box1.hashCode());
+        System.out.println(box2.hashCode());
 
+        System.out.println(box1.equals(box2));
     }
 }
