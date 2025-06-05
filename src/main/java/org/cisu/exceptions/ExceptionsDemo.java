81108cd15bacd7ea31355182a12cff92e05cf9d6
giorgos-grekis <georgrekis.github@gmail.com>
2025-06-05 16:07:22 +0300

    catching exceptions

--- src/main/java/org/cisu/exceptions/ExceptionsDemo.java
+++ src/main/java/org/cisu/exceptions/ExceptionsDemo.java
@@ -1,17 +1,38 @@
 package org.cisu.exceptions;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 /**
  * Three Types of Exceptions
- * Checked => at compile0 time "new FileReader("file.txt")"
- * Unchecked => not at compile time "sayHello(null)"
+ * Checked => not at compile time "new FileReader("file.txt")"
+ * Unchecked =>  at compile time "sayHello(null)"
  * Error
+ */
+
+/**
+ *  Exceptions Hierarchy
+ *  Throwable
+ *  Exception (checked) - Error
+ *  RuntimeException (unchecked)
  */
 
 public class ExceptionsDemo {
     public static void show() {
-//        var reader = new FileReader("file.txt");
+        try {
+            var reader = new FileReader("file.txt");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+//        try {
+//            var reader = new FileReader("file.txt");
+//            System.out.println("File opened");
+//        } catch (FileNotFoundException ex) {
+//            System.out.println(ex.getMessage());
+//        }
+
     }
 
     public static void sayHello(String name) {
