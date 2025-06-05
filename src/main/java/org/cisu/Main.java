13d8edeca6e37429a59b2b333249520e86450448
giorgos-grekis <georgrekis.github@gmail.com>
2025-06-05 14:55:12 +0300

    What are Exceptions

--- src/main/java/org/cisu/Main.java
+++ src/main/java/org/cisu/Main.java
@@ -1,11 +1,12 @@
 package org.cisu;
 
 
+import org.cisu.exceptions.ExceptionsDemo;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        ExceptionsDemo.show();
     }
 
 }
