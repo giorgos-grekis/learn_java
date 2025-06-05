5d2c97abc3ee31ebc22696cc610f20dc75a8cb7b
giorgos-grekis <georgrekis.github@gmail.com>
2025-06-05 16:24:15 +0300

    Throwing Exceptions

--- src/main/java/org/cisu/exceptions/Account.java
+++ src/main/java/org/cisu/exceptions/Account.java
@@ -1,10 +1,13 @@
 package org.cisu.exceptions;
 
+import javax.security.auth.login.AccountException;
+import java.io.IOException;
+
 public class Account {
-    public void deposit(float value) {
+    public void deposit(float value) throws IOException {
         // defencive programming
         if (value <= 0) {
-            throw new IllegalArgumentException("Value must be greater than zero");
+            throw new IOException("Value must be greater than zero");
         }
     }
 }
