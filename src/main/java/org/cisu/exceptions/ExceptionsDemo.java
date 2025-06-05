cb9bbab54a6fae03086a5df90a2b9687b4076d52
giorgos-grekis <georgrekis.github@gmail.com>
2025-06-05 16:01:17 +0300

    Types of exceptions

--- src/main/java/org/cisu/exceptions/ExceptionsDemo.java
+++ src/main/java/org/cisu/exceptions/ExceptionsDemo.java
@@ -1,8 +1,17 @@
 package org.cisu.exceptions;
 
+import java.io.FileReader;
+
+/**
+ * Three Types of Exceptions
+ * Checked => at compile0 time "new FileReader("file.txt")"
+ * Unchecked => not at compile time "sayHello(null)"
+ * Error
+ */
+
 public class ExceptionsDemo {
     public static void show() {
-        sayHello(null);
+//        var reader = new FileReader("file.txt");
     }
 
     public static void sayHello(String name) {
