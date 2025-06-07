49a7a627baf28e7cb000c6e56520b8a7472abac8
giorgos-grekis <georgrekis.github@gmail.com>
2025-06-07 21:18:05 +0300

    Anonymous Inner Classes

--- src/main/java/org/cisu/lambdas/LambdasDemo.java
+++ src/main/java/org/cisu/lambdas/LambdasDemo.java
@@ -2,7 +2,12 @@ package org.cisu.lambdas;
 
 public class LambdasDemo {
     public static void show() {
-        greet(new ConsolePrinter());
+        greet(new Printer() {
+            @Override
+            public void print(String message) {
+                System.out.println(message);
+            }
+        });
     }
 
 
