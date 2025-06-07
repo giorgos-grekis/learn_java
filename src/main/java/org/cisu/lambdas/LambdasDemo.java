cb2eb58d41609d1c90d61e5b9ca88ccc4b210a97
giorgos-grekis <georgrekis.github@gmail.com>
2025-06-07 21:23:33 +0300

    Lambda Expressions

--- src/main/java/org/cisu/lambdas/LambdasDemo.java
+++ src/main/java/org/cisu/lambdas/LambdasDemo.java
@@ -2,6 +2,11 @@ package org.cisu.lambdas;
 
 public class LambdasDemo {
     public static void show() {
+//        greet(message -> System.out.println(message));
+        greet(System.out::println);
+
+
+
         greet(new Printer() {
             @Override
             public void print(String message) {
--- src/main/java/org/cisu/lambdas/Printer.java
+++ src/main/java/org/cisu/lambdas/Printer.java
@@ -3,8 +3,8 @@ package org.cisu.lambdas;
 public interface Printer {
     void print(String message);
 
-    default void printTwice(String message) {
-        System.out.println(message);
-        System.out.println(message);
-    }
+//    default void printTwice(String message) {
+//        System.out.println(message);
+//        System.out.println(message);
+//    }
 }
