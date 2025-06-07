4accc2d6f09f8be057815e901a88f929493ed335
giorgos-grekis <georgrekis.github@gmail.com>
2025-06-07 21:32:02 +0300

    method references

--- src/main/java/org/cisu/lambdas/LambdasDemo.java
+++ src/main/java/org/cisu/lambdas/LambdasDemo.java
@@ -4,10 +4,19 @@ public class LambdasDemo {
     public  String prefix = "-";
 
     public void show() {
+        // Lambda Expressions
         greet(message -> System.out.println(this.prefix + message));
 
 
+        // Method References
+        // Class/Object::method
+        greet(this::print);
+//        greet(LambdasDemo::print); // if it's static
 
+
+
+
+//      // functional Interfaces
 //        greet(new Printer() {
 //            @Override
 //            public void print(String message) {
@@ -22,4 +31,7 @@ public class LambdasDemo {
     }
 
 
+    private void print(String message) {
+        System.out.println(this.prefix + message);
+    }
 }
