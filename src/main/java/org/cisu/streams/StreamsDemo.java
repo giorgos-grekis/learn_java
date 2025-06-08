9a5562b1749706a9e36c7dd430f99251bf2c5486
giorgos-grekis <georgrekis.github@gmail.com>
2025-06-08 16:47:43 +0300

    Slicing Streams

--- src/main/java/org/cisu/streams/StreamsDemo.java
+++ src/main/java/org/cisu/streams/StreamsDemo.java
@@ -8,22 +8,42 @@ import java.util.stream.Stream;
 public class StreamsDemo {
 
     public static void show() {
-
-        // Mapping Elements
         List<Movie> movies = List.of(
             new Movie("a", 10),
             new Movie("b", 15),
             new Movie("c", 20)
         );
 
-        movies.stream()
-                .map(Movie::getTitle)
-                .forEach(System.out::println);
 
-        var stream = Stream.of(List.of(1, 2, 3), List.of(4,5,6));
-        stream
-                .flatMap(Collection::stream)
-                .forEach(System.out::println);
+        // Slicing Streams
+        // 1000 moives
+        // 10 movies per page
+        // 3rd page
+        // skip(20) = skip ( (page -1) x pageSize )
+        // limit(10) = limit(pageSize)
+        movies.stream()
+                .takeWhile(movie -> movie.getLikes() < 30)
+                .dropWhile(movie -> movie.getLikes() < 30)
+                .skip(2)
+                .limit(10)
+                .forEach(m -> System.out.println(m.getTitle()));
+
+//        // Filtering Element
+//
+//        movies.stream()
+//                .filter(movie -> movie.getLikes() > 10)
+//                 .forEach(m -> System.out.println(m.getTitle()));
+
+
+//        // Mapping Elements
+//        movies.stream()
+//                .map(Movie::getTitle)
+//                .forEach(System.out::println);
+//
+//        var stream = Stream.of(List.of(1, 2, 3), List.of(4,5,6));
+//        stream
+//                .flatMap(Collection::stream)
+//                .forEach(System.out::println);
 
 //        // Creating a Stream
 //        Stream.generate(() -> "Hello").limit(10).forEach(System.out::println);
@@ -33,12 +53,7 @@ public class StreamsDemo {
 //                .forEach(n -> System.out.println(n));
 
 
-//        List<Movie> movies = List.of(
-//                new Movie("a", 10),
-//                new Movie("b", 15),
-//                new Movie("c", 20)
-//        );
-//
+
 //        int count = 0;
 //        for (var movie: movies)
 //            if (movie.getLikes() > 10)
