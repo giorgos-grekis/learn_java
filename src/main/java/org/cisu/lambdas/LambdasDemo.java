6cfa4093ec5b728416415dc88dfb3a65bf097517
giorgos-grekis <georgrekis.github@gmail.com>
2025-06-08 14:20:16 +0300

    The Supplier Interface

--- src/main/java/org/cisu/lambdas/LambdasDemo.java
+++ src/main/java/org/cisu/lambdas/LambdasDemo.java
@@ -3,19 +3,24 @@ package org.cisu.lambdas;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class LambdasDemo {
     public  String prefix = "-";
 
     public void show() {
 
-        // Chaining Consumer
+        // The Supplier Interface
+        Supplier<Double> getRandom = Math::random;
+        var random = getRandom.get();
+        System.out.println(random);
 
-        List<String> list = List.of("a", "b", "c");
-//        Consumer<String> print = (String item) -> System.out.println(item);
-        Consumer<String> print = System.out::println;
-        Consumer<String> printUpperCase = item -> System.out.println(item.toUpperCase());
-        list.forEach(print.andThen(printUpperCase).andThen(print));
+//        // Chaining Consumer
+//        List<String> list = List.of("a", "b", "c");
+////        Consumer<String> print = (String item) -> System.out.println(item);
+//        Consumer<String> print = System.out::println;
+//        Consumer<String> printUpperCase = item -> System.out.println(item.toUpperCase());
+//        list.forEach(print.andThen(printUpperCase).andThen(print));
 
 //        // Lambda Expressions
 //        greet(message -> System.out.println(this.prefix + message));
