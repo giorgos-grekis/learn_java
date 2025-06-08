c5d23727231e80404b30907c77f67b59b19e454c
giorgos-grekis <georgrekis.github@gmail.com>
2025-06-08 18:52:27 +0300

    Joining a Thread

--- src/main/java/org/cisu/concurrency/ThreadDemo.java
+++ src/main/java/org/cisu/concurrency/ThreadDemo.java
@@ -5,10 +5,18 @@ public class ThreadDemo {
 
         System.out.println(Thread.currentThread().getName());
 
-        for (var i = 0; i < 10; i++) {
+//        for (var i = 0; i < 10; i++) {
             Thread thread = new Thread(new DownloadFileTask());
             thread.start();
-        }
+
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println("File is ready to be scanned.");
+//        }
 
     }
 }
