785c7c50c0c0f9aba45a1eb4bebcbf0581f439cf
giorgos-grekis <georgrekis.github@gmail.com>
2025-06-08 18:50:16 +0300

    Pausing a Thread

--- src/main/java/org/cisu/concurrency/DownloadFileTask.java
+++ src/main/java/org/cisu/concurrency/DownloadFileTask.java
@@ -4,5 +4,14 @@ public class DownloadFileTask implements Runnable {
     @Override
     public void run() {
         System.out.println("Downloading a file: " + Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        System.out.println("Download complete: " + Thread.currentThread().getName());
     }
 }
