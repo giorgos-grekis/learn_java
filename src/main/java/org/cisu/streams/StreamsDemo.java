1382a07a4378bb6a8353cbc2c4ff57ef88df2881
giorgos-grekis <georgrekis.github@gmail.com>
2025-06-08 16:53:55 +0300

    distinct => Getting Unique Elements

--- src/main/java/org/cisu/streams/StreamsDemo.java
+++ src/main/java/org/cisu/streams/StreamsDemo.java
@@ -11,15 +11,23 @@ public class StreamsDemo {
     public static void show() {
         List<Movie> movies = List.of(
             new Movie("a", 10),
+            new Movie("a", 10),
             new Movie("b", 15),
             new Movie("c", 20)
         );
 
-        // Sorting Streams
+        // distinct => Getting Unique Elements
         movies.stream()
-//                .sorted((a,b) -> a.getTitle().compareTo(b.getTitle()))
-                .sorted(Comparator.comparing(Movie::getTitle))
-                .forEach(m-> System.out.println(m.getTitle()));
+                .map(Movie::getLikes)
+                .distinct()
+                .forEach(System.out::println);
+
+
+//        // Sorting Streams
+//        movies.stream()
+////                .sorted((a,b) -> a.getTitle().compareTo(b.getTitle()))
+//                .sorted(Comparator.comparing(Movie::getTitle))
+//                .forEach(m-> System.out.println(m.getTitle()));
 
 //        // Slicing Streams
 //        // 1000 moives
