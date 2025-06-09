99b9a511f6a46b1bc966819cb39a6779c220a415
giorgos-grekis <georgrekis.github@gmail.com>
2025-06-09 18:09:43 +0300

    Transforming a Completable future

--- src/main/java/org/cisu/executors/CompletableFutureDemo.java
+++ src/main/java/org/cisu/executors/CompletableFutureDemo.java
@@ -7,22 +7,33 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.function.Supplier;
 
 public class CompletableFutureDemo {
+
+    public static int toFahrenheit(int celsius) {
+        return (int) (celsius * 1.8) + 32;
+    }
+
     public static void show() {
 
-        // Handling Exceptions
-        var future = CompletableFuture.supplyAsync(() -> {
-            System.out.println("Getting the current weather");
-            throw new IllegalStateException();
-        });
+        // Transforming a Completable future
+        var future = CompletableFuture.supplyAsync(() -> 20);
+        future
+                .thenApply(CompletableFutureDemo::toFahrenheit)
+                .thenAccept(System.out::println);
 
-        try {
-            var temperature = future.exceptionally(ex -> 1).get();
-            System.out.println(temperature);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
-        }
+//        // Handling Exceptions
+//        var future = CompletableFuture.supplyAsync(() -> {
+//            System.out.println("Getting the current weather");
+//            throw new IllegalStateException();
+//        });
+//
+//        try {
+//            var temperature = future.exceptionally(ex -> 1).get();
+//            System.out.println(temperature);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        } catch (ExecutionException e) {
+//            throw new RuntimeException(e);
+//        }
 
 //        // Running Code on Completion
 //        var future = CompletableFuture.supplyAsync(() -> 1);
